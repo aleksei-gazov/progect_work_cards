0eca1c9112d0ee14efb9c4af66ef4db70d99b4be
aleksei-gazov <98910189+aleksei-gazov@users.noreply.github.com>
2023-04-06 15:27:21 +0300

    create header

---
+++ header/Header.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './Header.scss';
+
+export const Header =() => {
+  return (
+    <div className='header'>
+     <div className ="logo">
+LOGO
+     </div>
+     <div className='info'>
+       <div className='info_data'>
+         <h5>Name</h5>
+         <p>Status</p>
+       </div>
+       <div className='avatar'>
+         Avatar
+       </div>
+     </div>
+    </div>
+  );
+}
